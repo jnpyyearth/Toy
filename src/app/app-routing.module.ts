e0abd77cbbd40f7cfe281be0be69447e72a63430
jnpyyearth <jetnipat.y@ku.th>
2024-08-29 14:44:06 +0700

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { CartComponent } from './cart/cart.component';
+import { AccountComponent } from './account/account.component';
+import { LogOutComponent } from './log-out/log-out.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' }, 
+  { path: 'home', component: HomeComponent }, 
+  { path: 'menu', component: MenuComponent },
+  { path: 'about-us', component: AboutUsComponent},
+  { path: 'inventory', component: InventoryComponent},
+  { path: 'contact-us', component: ContactUsComponent},
+  { path: 'cart', component: CartComponent},
+  { path : 'account', component: AccountComponent},
+  { path : 'log-out', component: LogOutComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { MenuComponent } from './menu/menu.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { InventoryComponent } from './inventory/inventory.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
+import { LogInComponent } from './log-in/log-in.component';
+import { CartComponent } from './cart/cart.component';
+import { AccountComponent } from './account/account.component';
+import { LogOutComponent } from './log-out/log-out.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MenuComponent,
+    AboutUsComponent,
+    InventoryComponent,
+    ContactUsComponent,
+    LogInComponent,
+    CartComponent,
+    AccountComponent,
+    LogOutComponent
   ],
   imports: [
     BrowserModule,
