8b9a2b0786deb85498b804a378fd5ccde8bd72a5
jnpyyearth <jetnipat.y@ku.th>
2024-08-31 03:48:15 +0700

    นรกฟอนต์เอนมีอยู่จริง

--- src/app/log-in/log-in.component.ts
+++ src/app/log-in/log-in.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-log-in',
   templateUrl: './log-in.component.html',
-  styleUrl: './log-in.component.css'
+  styleUrls: ['./log-in.component.css' ]
 })
 export class LogInComponent {
 
