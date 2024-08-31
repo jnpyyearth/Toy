0a94ecfa9a7ce9cf4878eff087439d99e5528a06
jnpyyearth <jetnipat.y@ku.th>
2024-08-31 14:22:39 +0700

    f

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LogInComponent } from './log-in/log-in.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
 import { AboutUsComponent } from './about-us/about-us.component';
@@ -8,12 +9,12 @@ import { ContactUsComponent } from './contact-us/contact-us.component';
 import { CartComponent } from './cart/cart.component';
 import { AccountComponent } from './account/account.component';
 import { LogOutComponent } from './log-out/log-out.component';
-import { LogInComponent } from './log-in/log-in.component';
-import { AppComponent } from './app.component';
+
+
 
 const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
-  { path: 'app', component: AppComponent }, 
+  { path : 'log-in', component: LogInComponent},
   { path: 'home', component: HomeComponent }, 
   { path: 'menu', component: MenuComponent },
   { path: 'about-us', component: AboutUsComponent},
@@ -22,7 +23,7 @@ const routes: Routes = [
   { path: 'cart', component: CartComponent},
   { path : 'account', component: AccountComponent},
   { path : 'log-out', component: LogOutComponent},
-  { path : 'log-in', component: LogInComponent},
+  
 ];
 
 @NgModule({
